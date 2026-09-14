94951af43f1c8a59346d62c3bf0529918a678544
Pawel Cepak <53912041+pawelcepak@users.noreply.github.com>
2026-09-14 18:20:20 +0200

    Bump app version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "pl.szynolandia.szybkaklawiatura"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     buildTypes {
