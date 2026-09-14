38bdaceb12f89caa6453114b074789816b4f5729
Pawel Cepak <53912041+pawelcepak@users.noreply.github.com>
2026-09-14 18:33:30 +0200

    Version 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "pl.szynolandia.szybkaklawiatura"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
     }
 
     buildTypes {
